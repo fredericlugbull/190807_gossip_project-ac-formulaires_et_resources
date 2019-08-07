d3f2e4bb4bbae2f32fe601701d310e0ce067e056
Frédéric <f.lugbull@gmail.com>
2019-08-07 14:46:03 +0200

    yo

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,25 @@
+class HomeController < ApplicationController
+  def home_page
+    @gossips = Gossip.all
+  end
+
+  def gossip_show
+    @gossip = Gossip.find(params[:id])
+  end
+
+  def landing_page
+    @first_name = params[:first_name]
+  end
+
+  def user_page
+    @user = User.find(params[:id])
+  end
+
+  def contact_show
+  end
+
+  def show_team
+  end
+
+
+end
